32649a56762f7ca8c32c55576aa4c58b5687d5e6
Eduardo Tavares <eduardotdev.21@gmail.com>
2024-02-11 13:50:59 -0300

    testes e controller de autenticacao

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,9 @@
 import { FastifyInstance } from 'fastify'
 
+import { authenticate } from './coontrollers/authenticate'
 import { register } from './coontrollers/register'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
 }
